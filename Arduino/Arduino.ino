995a98227d012a1b5dbf655ad3f2e9fc9bcdb381
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-05-14 11:07:43 +0800

    Update comments

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -10,17 +10,18 @@ Servo rotatingBack;
 Servo leftRight;
 Servo upDown;
 
+// VARIABLES
 int pos = 90;       // variable to store the servo position
 int ByteReceived;   // store byte recieved via serial
 
-
+// INITIALIZE
 void setup() {
   throttle.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
   throttle.write(pos);
 }
 
-
+// UPDATE
 void loop() {
   if (Serial.read() != -1) {
     ByteReceived = Serial.read();
